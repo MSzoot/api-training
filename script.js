b9794133250bb220e4e211dabd6b1310e2fc930e
Marcin Szote <Marcinszoot@gmail.com>
2022-11-08 14:49:34 +0000

    async await

--- script.js
+++ script.js
@@ -2,15 +2,26 @@ const button = document.getElementById("button")
 const dogImg = document.getElementById("dogImg")
 
 
-const getDog = () => {
-fetch("https://dog.ceo/api/breeds/image/random")
-.then(response => response.json())
-.then(json => {
-    console.log(json.message)
-    dogImg.innerHTML =`<img src='${json.message}'/>`
-})
-}
+// const getDog = () => {
+// fetch("https://dog.ceo/api/breeds/image/random")
+// .then(response => response.json())
+// .then(json => {
+//     dogImg.innerHTML =`<img src='${json.message}'/>`
+// })
+// }
+
+
+//async await attempt
 
 button.onclick = () => {
-   getDog();
+   getDog2();
 }
+
+const getDog2 = async () => {
+    const url = "https://dog.ceo/api/breeds/image/random"
+    const response = await fetch(url);
+    const data = await response.json()
+    dogImg.innerHTML =`<img src='${data.message}'/>`
+    
+}
+
